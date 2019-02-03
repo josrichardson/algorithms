309333f93ba4b60df0f8568ef4b8cd68dc3e2d4a
josrichardson <richardson.s.jo@gmail.com>
2019-02-03 18:01:34 -0400

    This is my first commit.

---
+++ FactorialTest.java
@@ -0,0 +1,18 @@
+public class FactorialTest{ 
+
+	public static void main(String[] args){
+
+		int num = 5; 
+		System.out.println("Factorial of "+ num + " is " + factorial(5)); 
+
+	} 
+
+	public static int factorial(int n){ 
+
+		if (n == 0) 
+		return 1; 
+		
+		return n * factorial(n-1); 
+
+	} 	
+} 
